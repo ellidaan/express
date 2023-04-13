388d63d556af6c32a1fd5d80237702dd4c43a929
ellidaan <elone.meimoun@epitech.digital>
2023-04-13 14:20:25 +0200

    first commit

---
+++ express.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000,() => {
+    console.log('Server started on port http://localhost:3000');
+    }
+    
+    ) 
+
+
+
+
+
+  
+      
